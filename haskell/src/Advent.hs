601fff2d78129440ecfb6f592ac764225c78d6ef
Pierre Thierry <pierre@nothos.net>
2024-12-13 03:18:46 +0100

    Refactor

--- haskell/src/Advent.hs
+++ haskell/src/Advent.hs
@@ -8,7 +8,7 @@ day1_1 :: String -> Int
 day1_1 input =
   input
     & lines
-    & map (words >>> map read >>> toPair)
+    & map (readNums >>> toPair)
     & unzip
     & sort2
     & uncurry (zipWith distance)
@@ -16,6 +16,9 @@ day1_1 input =
  where
   sort2 (list1, list2) = (sort list1, sort list2)
   distance a b = abs $ a - b
+
+readNums :: (Read a) => String -> [a]
+readNums = words >>> map read
 
 toPair [a, b] = (a, b)
 
@@ -25,34 +28,28 @@ day1_2 input =
     & map (similarity 0 list2)
     & sum
  where
-  (list1, list2) = input & lines & map (toPair . map read . words) & unzip
+  (list1, list2) = input & lines & map (readNums >>> toPair) & unzip
   similarity :: Int -> [Int] -> Int -> Int
   similarity count [] num = count * num
   similarity count (other : rest) num = similarity (count + (if num == other then 1 else 0)) rest num
 
 day2_1 :: String -> Int
-day2_1 input =
-  input
-    & lines
-    & map (words >>> map read >>> isSafe)
-    & filter id
-    & length
- where
-  isSafe (x : y : rest) = go (compare x y) (x : y : rest)
-  go prevDir (x : y : rest) =
-    (curDir /= EQ && curDir == prevDir && abs (x - y) < 4) && (null rest || go curDir (y : rest))
-   where
-    curDir = compare x y
+day2_1 = day2 isSafe
 
 day2_2 :: String -> Int
-day2_2 input =
+day2_2 = day2 $ allRemovals >>> any isSafe
+
+day2 :: (Num a, Ord a, Read a) => ([a] -> Bool) -> String -> Int
+day2 checkSafe input =
   input
     & lines
-    & map (words >>> map read >>> allRemovals >>> any isSafe)
+    & map (readNums >>> checkSafe)
     & filter id
     & length
+
+isSafe :: (Num a, Ord a) => [a] -> Bool
+isSafe (x : y : rest) = go (compare x y) (x : y : rest)
  where
-  isSafe (x : y : rest) = go (compare x y) (x : y : rest)
   go prevDir (x : y : rest) =
     (curDir /= EQ && curDir == prevDir && abs (x - y) < 4) && (null rest || go curDir (y : rest))
    where
