a7df8477a0085996fb3ee7a4cd7ffdf40cd3c3ca
Pierre Thierry <pierre@nothos.net>
2024-12-13 02:00:01 +0100

    Day 2

--- haskell/src/Advent.hs
+++ haskell/src/Advent.hs
@@ -29,3 +29,41 @@ day1_2 input =
   similarity :: Int -> [Int] -> Int -> Int
   similarity count [] num = count * num
   similarity count (other : rest) num = similarity (count + (if num == other then 1 else 0)) rest num
+
+day2_1 :: String -> Int
+day2_1 input =
+  input
+    & lines
+    & map (words >>> map read >>> isSafe)
+    & filter id
+    & length
+ where
+  isSafe (x : y : rest) = go (compare x y) (x : y : rest)
+  go prevDir (x : y : rest) =
+    (curDir /= EQ && curDir == prevDir && abs (x - y) < 4) && (null rest || go curDir (y : rest))
+   where
+    curDir = compare x y
+
+day2_2 :: String -> Int
+day2_2 input =
+  input
+    & lines
+    & map (words >>> map read >>> allRemovals >>> any isSafe)
+    & filter id
+    & length
+ where
+  isSafe (x : y : rest) = go (compare x y) (x : y : rest)
+  go prevDir (x : y : rest) =
+    (curDir /= EQ && curDir == prevDir && abs (x - y) < 4) && (null rest || go curDir (y : rest))
+   where
+    curDir = compare x y
+
+allRemovals :: [a] -> [[a]]
+allRemovals list = do
+  removeIndex list <$> [1 .. (length list)]
+
+removeIndex :: [a] -> Int -> [a]
+removeIndex [] _ = []
+removeIndex l 0 = l
+removeIndex (x : xs) 1 = xs
+removeIndex (x : xs) n = x : removeIndex xs (n - 1)
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -8,3 +8,7 @@ main = hspec do
       (day1_1 <$> readFile "test/day1.test") `shouldReturn` 11
     it "day 1.2" do
       (day1_2 <$> readFile "test/day1.test") `shouldReturn` 31
+    it "day 2.1" do
+      (day2_1 <$> readFile "test/day2.test") `shouldReturn` 2
+    it "day 2.2" do
+      (day2_2 <$> readFile "test/day2.test") `shouldReturn` 4
